e46ee378cb64ec01bc11fbea5f2277c2a1db14f6
Jason Harwig <jharwig@nic-jharwig.local>
2009-09-15 02:14:35 -0400

    ignore build directory

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Clock
+//
+//  Created by Jason Harwig on 6/15/09.
+//  Copyright 2009 Near Infinity Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
